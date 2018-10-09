7f0fd02be4f51869fba51be2d081c5db2d982aeb
kvraviteja93 <kvraviteja93@gmail.com>
2018-10-09 18:32:56 +0100

    first commit

---
+++ knear.py
@@ -0,0 +1,73 @@
+#Importing The Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Importing The Dataset
+dataset=pd.read_csv("Social_Network_Ads.csv")
+
+#Separating Independent and Dependent Variables
+X=dataset.iloc[:,[2,3]].values
+y=dataset.iloc[:,-1].values
+
+#Splitting The Dataset Into Training and Test Set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.fit_transform(X_test)
+
+#Fitting The K-NN Classifier to The Training Set
+from sklearn.neighbors import KNeighborsClassifier
+clf=KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
+clf.fit(X_train,y_train)
+
+#Predicting The Test Set Results
+y_pred=clf.predict(X_test)
+
+#Checking The Accuracy
+from sklearn.metrics import accuracy_score,confusion_matrix
+acc=accuracy_score(y_test,y_pred)
+con=confusion_matrix(y_test,y_pred)
+
+#Visualizing The Training Set Results
+%matplotlib qt5
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN(Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#Visualizing The Test Set Results
+%matplotlib qt5
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clf.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
